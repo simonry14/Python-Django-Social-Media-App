89ac5c1e3d42dd86d2e2921a5475baf3933c463b
Simon Peter Miyingo <simonpetermiyingo@gmail.com>
2022-02-12 23:44:03 +0300

    profiles

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,3 +1,15 @@
+from dataclasses import fields
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
 # Register your models here.
+
+class UserAdmin (admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)

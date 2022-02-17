45ff4ca8267ce71233253c936559144553e50e80
Simon Peter Miyingo <simonpetermiyingo@gmail.com>
2022-02-17 08:40:32 +0300

    dweet model form

---
+++ dwitter/forms.py
@@ -0,0 +1,10 @@
+import imp
+from django import forms
+from .models import Dweet
+
+class DweetForm(forms.ModelForm):
+    body = forms.CharField(required=True)
+    
+    class Meta:
+        model = Dweet
+        exclude = ("user", )

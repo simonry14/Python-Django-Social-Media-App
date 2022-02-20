f8a6d1dfd3bfc24800470a56f2f739fe08fe1ed0
Simon Peter Miyingo <simonpetermiyingo@gmail.com>
2022-02-20 08:40:21 +0300

    postgre

--- social/settings.py
+++ social/settings.py
@@ -81,6 +81,17 @@ DATABASES = {
     }
 }
 
+#DATABASE = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'blog',
+    #    'USER': 'admin',
+     #   'PASSWORD': '123456',
+      #  'HOST': 'localhost',
+       # 'PORT': '5432'
+        #}
+    #}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
